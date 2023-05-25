7eef7f9d1fbb8fddb2428a13c3007c00f61ca0f1
Timothée DURAND <timothee.durand70@gmail.com>
2023-05-25 11:54:28 +0200

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import glsl from 'vite-plugin-glsl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.MODE === "production" ? '/alestorm' : '/',
+  base: import.meta.env.MODE === "production" ? '/Alestorm' : '/',
   plugins: [vue(), glsl()],
 })
